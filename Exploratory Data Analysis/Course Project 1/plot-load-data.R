db12434507b4b2a97db247cba8e419c417131436
Andrei Dumitrescu <andrei@codemachiner.com>
2014-10-12 06:25:15 +0300

    last minute refactoring

--- Exploratory Data Analysis/Course Project 1/plot-load-data.R
+++ Exploratory Data Analysis/Course Project 1/plot-load-data.R
@@ -5,11 +5,19 @@ setwd('/repository-ssd/work-2.0/datasciencecoursera/Exploratory\ Data\ Analysis/
 library(dplyr)
 
 #
+# Define some formats so we don't repeat ourselves
+#
+DATE_FORMAT = "%d/%m/%Y"
+DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+START_DATE = "01/02/2007"
+END_DATE = "02/02/2007"
+
+#
 loadWattageData <- function() {
 
   # create a new format for coercing the Date column
   setAs("character", "customDate", function(from) {
-    as.Date(from, format = "%d/%m/%Y")
+    as.Date(from, format = DATE_FORMAT)
   })
 
   # presuming data file is in ./data
@@ -27,14 +35,6 @@ loadWattageData <- function() {
 }
 
 #
-# CONST
-#
-DATE_FORMAT = "%d/%m/%Y"
-DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
-START_DATE = "01/02/2007"
-END_DATE = "02/02/2007"
-
-#
 # READ DATA FILE
 #
 # It's a big file, load it once and keep it unaltered.
@@ -42,20 +42,20 @@ END_DATE = "02/02/2007"
 #
 if (!exists("wattageDataSource")) {
   wattageDataSource <- loadWattageData()
-
-  #
-  # FILTER OUT
-  #
-  # Get the data subset that we need, from 01/02/2007 to 01/02/2007.
-  # Using the same custom date coercer.
-  #
-  wattageSubset <- wattageDataSource %>%
-    mutate(DateTime = paste(Date, Time)) %>%
-    filter(
-      Date >= as.Date(START_DATE, format = DATE_FORMAT),
-      Date <= as.Date(END_DATE, format = DATE_FORMAT)
-    ) %>%
-    select(-Date, -Time)
-
-  wattageSubset$DateTime <- strptime(wattageSubset$DateTime, DATE_TIME_FORMAT)
 }
+
+#
+# FILTER OUT
+#
+# Get the data subset that we need, from 01/02/2007 to 01/02/2007.
+# Using the same custom date coercer.
+#
+wattageSubset <- wattageDataSource %>%
+  mutate(DateTime = paste(Date, Time)) %>%
+  filter(
+    Date >= as.Date(START_DATE, format = DATE_FORMAT),
+    Date <= as.Date(END_DATE, format = DATE_FORMAT)
+  ) %>%
+  select(-Date, -Time)
+
+wattageSubset$DateTime <- strptime(wattageSubset$DateTime, DATE_TIME_FORMAT)
